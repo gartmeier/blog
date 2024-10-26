5bcdb2a9c453b75fce5761cea8285b33c73f5505
Joshua Gartmeier <joshua@gartmeier.dev>
2024-10-26 06:55:12 +0200

    Add inline-styles for size and text

--- blog/admin.py
+++ blog/admin.py
@@ -1,12 +1,27 @@
 from django.contrib import admin
 from django.contrib.admin import register
+from django.forms import widgets
+from django.db import models
 
 from blog.models import Post, Image
 
 
 @register(Post)
-class ArticleAdmin(admin.ModelAdmin):
-    pass
+class PostAdmin(admin.ModelAdmin):
+    formfield_overrides = {
+        models.TextField: {
+            "widget": widgets.Textarea(
+                attrs={
+                    "style": """
+                        width: 120ch;
+                        height: 50ch;
+                        font-size: 20px;
+                        line-height: 1.5;
+                    """
+                }
+            )
+        },
+    }
 
 
 @register(Image)
