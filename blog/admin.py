3b3c5d9b823fa4009862cdfb6f73b827a8699669
Joshua Gartmeier <joshua@gartmeier.dev>
2024-10-26 06:57:55 +0200

    Reformat

--- blog/admin.py
+++ blog/admin.py
@@ -12,12 +12,7 @@ class PostAdmin(admin.ModelAdmin):
         models.TextField: {
             "widget": widgets.Textarea(
                 attrs={
-                    "style": """
-                        width: 120ch;
-                        height: 50ch;
-                        font-size: 20px;
-                        line-height: 1.5;
-                    """
+                    "style": "width: 120ch; height: 50ch; font-size: 20px; line-height: 1.5"
                 }
             )
         },
